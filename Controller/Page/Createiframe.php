99c2817b9f458d1bd6791144b3f974f60d2a7651
zaheercena <zaheercena@gmail.com>
2022-02-10 17:23:26 +0500

    Fixed Merchant ID Issue

--- Controller/Page/Createiframe.php
+++ Controller/Page/Createiframe.php
@@ -122,12 +122,65 @@ class createiframe extends \Magento\Framework\App\Action\Action
           ),
         ));
 
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-
-        $result = $this->resultJsonFactory->create();
-
-        return $result->setData(json_decode($response, 1));
+          $ch = curl_init();
+          curl_setopt($ch, CURLOPT_URL,$url);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+          curl_setopt($ch, CURLOPT_CUSTOMREQUEST,'POST');
+          curl_setopt($ch, CURLOPT_ENCODING,'');
+          curl_setopt($ch, CURLOPT_MAXREDIRS,10);
+          curl_setopt($ch, CURLOPT_TIMEOUT,0);
+          curl_setopt($ch, CURLOPT_FOLLOWLOCATION,true);
+          curl_setopt($ch, CURLOPT_HTTP_VERSION,CURL_HTTP_VERSION_1_1);
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'sec-ch-ua: "Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+            'sec-ch-ua-mobile: ?0',
+            'Authorization: Basic '.$key,
+            'Content-Type: application/json',
+            'Accept: */*',
+            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+            'sec-ch-ua-platform: "Windows"',
+            'Cookie: XSRF-TOKEN=eyJpdiI6Im1XQkpKd3lYbTlObDFBRjk1NFo0S1E9PSIsInZhbHVlIjoiTWJKNEJoMlZTSUFsOStCSEp6WWpJdWJnZzh6VkViL0FJbjJvRW5JVm96MlN5a0lRQkJ6U2hScmZES3RoSHdOeEJxTjVZTWxPWWpCNHQydVplemwzOFBTcjIyVVp5Y2VNY2JPMEp3ZFF1M0YzWXNSeDYyekxCN3ppNGNHdTlMTlEiLCJtYWMiOiI2OGZhMzM0NDYzOWZkZjFhYTdiYjU3Yzg0ZTAzYjcxYjMxNDdhYWYxNDYxY2FiN2U1YmM5ZGI0ZTgyMzhhOWM4IiwidGFnIjoiIn0%3D; qisstpay_sandbox_session=eyJpdiI6IklpVnRIVkhxYW1YWDM3cUZGNHB1V0E9PSIsInZhbHVlIjoialdYNUxjUllzR3JhMDJuUVZ5SHRzdUt1N21VYVpYcE9neGZRN2pQYU4yS0lEWGtNaFFoSjFIWlI4aERNa2hEYVJiOFlSUVZicW84blBVQlBWaXlHWElEQitzTlNFcGExWjZld3FuOGRSSFQyMmo2R2Vmb2VFUHhNOGhSTmRaREYiLCJtYWMiOiJiZDdjNTYyOWVlN2I5YjAyMTZjNTllY2NlOGJiYTA1YjgyOTRmYTA4NTllMTE0OWYxZDQ2NmY3NmE5NmMwMWE0IiwidGFnIjoiIn0%3D'
+          ));
+          $jayParsedAry = '{
+            "partner_id":"magento",
+            "fname":"'.$cart_data['firstname'].'",
+            "mname":"",
+            "lname":"'.$cart_data['lastname'].'",
+            "email":"",
+            "ip_addr":"'.$remote->getRemoteAddress().'",
+            "shipping_info":{
+              "addr1":"'.$cart_data['street'].'",
+              "addr2":"",
+              "state":"'.$cart_data['region'].'",
+              "city":"'.$cart_data['city'].'",
+              "zip":"'.$cart_data['postcode'].'"
+            },
+            "billing_info":{
+              "addr1":"'.$cart_data['street'].'",
+              "addr2":"",
+              "state":"'.$cart_data['region'].'",
+              "city":"'.$cart_data['city'].'",
+              "zip":"'.$cart_data['postcode'].'"
+            },
+            "shipping_details":{},
+            "card_details":{},
+            "itemFlag":false,
+            "line_items":[],
+            "merchant_order_id": "'.$orderId.'",
+            "total_amount":'.$cart_data['grand_total'].'
+          }';
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $jayParsedAry);
+          $server_output = curl_exec($ch);
+          curl_close ($ch);
+          $response = curl_exec($curl);
+          $result = curl_close($curl);
+          $result = $this->resultJsonFactory->create();
+          // $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/qistpay.log');
+          // $logger = new \Zend\Log\Logger();
+          // $logger->addWriter($writer);
+          // $logger->info('Your text message');
+          // $logger->info($server_output);
+          // $logger->info($response);
+          return $result->setData(json_decode($response));
     }
 }
